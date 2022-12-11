c749a66f211021d175479ef260698e89a04e1a96
Ignacio Molina <ignaciotmolina@gmail.com>
2022-12-11 11:43:14 -0300

    agrupando casos de prueba

--- unit_test/__test__/calc.test.js
+++ unit_test/__test__/calc.test.js
@@ -1,5 +1,14 @@
+// const { describe } = require('yargs');
 const suma = require('../calc.js')
 
-test('Suma de dos numeros, ',() =>{
-    expect(suma(1,2)).toBe(3); 
-})
+
+describe('Prueba de sumas',() => {
+    test('Suma de dos numeros, ',() =>{
+        expect(suma(1,2)).toBe(3); 
+    })
+    
+    test('Suma de ceros, ',() =>{
+        expect(suma(0,0)).toBe(0); 
+    })
+});
+
